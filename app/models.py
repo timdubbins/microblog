2693f1c43801c487575ccefd93aab6ebf43c065c
tidub <57918941+tidub@users.noreply.github.com>
2020-11-19 15:30:02 +0000

    completed private-messaging feature

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from time import time
 from flask import current_app
@@ -86,10 +87,12 @@ class User(UserMixin, db.Model):
     messages_sent = db.relationship('Message',
                                     foreign_keys='Message.sender_id',
                                     backref='author', lazy='dynamic')
-    messages_recieved = db.relationship('Message',
+    messages_received = db.relationship('Message',
                                         foreign_keys='Message.recipient_id',
                                         backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
 
 
     def __repr__(self):
@@ -145,6 +148,12 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
 
 class Post(SearchableMixin, db.Model):
     __searchable__ = ['body']
@@ -160,3 +169,14 @@ class Post(SearchableMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
