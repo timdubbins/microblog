3c3bea99b08fc564cd326b16df58c51ef8a89b4a
tidub <57918941+tidub@users.noreply.github.com>
2020-12-01 20:24:56 +0000

    completed export-posts feature

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,8 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from elasticsearch import Elasticsearch
 import logging
 import os
+from redis import Redis
+import rq
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -36,6 +38,8 @@ def create_app(config_class=Config):
     babel.init_app(app)
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
+    app.redis = Redis.from_url(app.config['REDIS_URL'])
+    app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
